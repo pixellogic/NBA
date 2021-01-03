bd32c7a761ebf168a5df84603398b6c4882268e8
Ashish Bajracharya <lsr.files1@gmail.com>
2021-01-03 10:19:29 +0545

    basic css reset done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {BrowserRouter} from 'react-router-dom'
+import './styles/styles.css'
 export default function App() {
     return (
-        <div>
-            app
-        </div>
+       <BrowserRouter>
+        app
+       </BrowserRouter>
     )
 }
